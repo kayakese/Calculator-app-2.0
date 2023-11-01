ae22f360d9c2043d1f39eb8eb478458229e07cc2
Renate <Renate@RENATE_PC>
2023-11-01 20:50:36 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,367 @@
+﻿namespace Calculator_app_2._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            TxtBox = new TextBox();
+            OneBtn = new Button();
+            FourBtn = new Button();
+            SevenBtn = new Button();
+            ZeroBtn = new Button();
+            CommaBtn = new Button();
+            EightBtn = new Button();
+            FiveBtn = new Button();
+            TwoBtn = new Button();
+            EqualsBtn = new Button();
+            NineBtn = new Button();
+            SixBtn = new Button();
+            ThreeBtn = new Button();
+            MinusPlusBtn = new Button();
+            DivideBtn = new Button();
+            MinusBtn = new Button();
+            ClearBtn = new Button();
+            ModuleBtn = new Button();
+            MultiplyBtn = new Button();
+            PlusBtn = new Button();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // TxtBox
+            // 
+            TxtBox.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            TxtBox.Location = new Point(13, 12);
+            TxtBox.Name = "TxtBox";
+            TxtBox.ReadOnly = true;
+            TxtBox.Size = new Size(448, 29);
+            TxtBox.TabIndex = 0;
+            TxtBox.Text = "0";
+            TxtBox.TextAlign = HorizontalAlignment.Right;
+            // 
+            // OneBtn
+            // 
+            OneBtn.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            OneBtn.ForeColor = Color.Black;
+            OneBtn.Location = new Point(20, 93);
+            OneBtn.Name = "OneBtn";
+            OneBtn.Size = new Size(69, 64);
+            OneBtn.TabIndex = 1;
+            OneBtn.Text = "1";
+            OneBtn.UseVisualStyleBackColor = true;
+            OneBtn.Click += OneBtn_Click;
+            // 
+            // FourBtn
+            // 
+            FourBtn.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            FourBtn.ForeColor = Color.Black;
+            FourBtn.Location = new Point(20, 163);
+            FourBtn.Name = "FourBtn";
+            FourBtn.Size = new Size(69, 64);
+            FourBtn.TabIndex = 2;
+            FourBtn.Text = "4";
+            FourBtn.UseVisualStyleBackColor = true;
+            FourBtn.Click += FourBtn_Click;
+            // 
+            // SevenBtn
+            // 
+            SevenBtn.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            SevenBtn.ForeColor = Color.Black;
+            SevenBtn.Location = new Point(20, 233);
+            SevenBtn.Name = "SevenBtn";
+            SevenBtn.Size = new Size(69, 64);
+            SevenBtn.TabIndex = 3;
+            SevenBtn.Text = "7";
+            SevenBtn.UseVisualStyleBackColor = true;
+            SevenBtn.Click += SevenBtn_Click;
+            // 
+            // ZeroBtn
+            // 
+            ZeroBtn.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            ZeroBtn.ForeColor = Color.Black;
+            ZeroBtn.Location = new Point(20, 303);
+            ZeroBtn.Name = "ZeroBtn";
+            ZeroBtn.Size = new Size(69, 64);
+            ZeroBtn.TabIndex = 4;
+            ZeroBtn.Text = "0";
+            ZeroBtn.UseVisualStyleBackColor = true;
+            ZeroBtn.Click += ZeroBtn_Click;
+            // 
+            // CommaBtn
+            // 
+            CommaBtn.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            CommaBtn.ForeColor = Color.Black;
+            CommaBtn.Location = new Point(95, 303);
+            CommaBtn.Name = "CommaBtn";
+            CommaBtn.Size = new Size(69, 64);
+            CommaBtn.TabIndex = 8;
+            CommaBtn.Text = ".";
+            CommaBtn.UseVisualStyleBackColor = true;
+            CommaBtn.Click += CommaBtn_Click;
+            // 
+            // EightBtn
+            // 
+            EightBtn.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            EightBtn.ForeColor = Color.Black;
+            EightBtn.Location = new Point(95, 233);
+            EightBtn.Name = "EightBtn";
+            EightBtn.Size = new Size(69, 64);
+            EightBtn.TabIndex = 7;
+            EightBtn.Text = "8";
+            EightBtn.UseVisualStyleBackColor = true;
+            EightBtn.Click += EightBtn_Click;
+            // 
+            // FiveBtn
+            // 
+            FiveBtn.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            FiveBtn.ForeColor = Color.Black;
+            FiveBtn.Location = new Point(95, 163);
+            FiveBtn.Name = "FiveBtn";
+            FiveBtn.Size = new Size(69, 64);
+            FiveBtn.TabIndex = 6;
+            FiveBtn.Text = "5";
+            FiveBtn.UseVisualStyleBackColor = true;
+            FiveBtn.Click += FiveBtn_Click;
+            // 
+            // TwoBtn
+            // 
+            TwoBtn.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            TwoBtn.ForeColor = Color.Black;
+            TwoBtn.Location = new Point(95, 93);
+            TwoBtn.Name = "TwoBtn";
+            TwoBtn.Size = new Size(69, 64);
+            TwoBtn.TabIndex = 5;
+            TwoBtn.Text = "2";
+            TwoBtn.UseVisualStyleBackColor = true;
+            TwoBtn.Click += TwoBtn_Click;
+            // 
+            // EqualsBtn
+            // 
+            EqualsBtn.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            EqualsBtn.ForeColor = Color.Black;
+            EqualsBtn.Location = new Point(170, 303);
+            EqualsBtn.Name = "EqualsBtn";
+            EqualsBtn.Size = new Size(69, 64);
+            EqualsBtn.TabIndex = 12;
+            EqualsBtn.Text = "=";
+            EqualsBtn.UseVisualStyleBackColor = true;
+            EqualsBtn.Click += EqualsBtn_Click;
+            // 
+            // NineBtn
+            // 
+            NineBtn.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            NineBtn.ForeColor = Color.Black;
+            NineBtn.Location = new Point(170, 233);
+            NineBtn.Name = "NineBtn";
+            NineBtn.Size = new Size(69, 64);
+            NineBtn.TabIndex = 11;
+            NineBtn.Text = "9";
+            NineBtn.UseVisualStyleBackColor = true;
+            NineBtn.Click += NineBtn_Click;
+            // 
+            // SixBtn
+            // 
+            SixBtn.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            SixBtn.ForeColor = Color.Black;
+            SixBtn.Location = new Point(170, 163);
+            SixBtn.Name = "SixBtn";
+            SixBtn.Size = new Size(69, 64);
+            SixBtn.TabIndex = 10;
+            SixBtn.Text = "6";
+            SixBtn.UseVisualStyleBackColor = true;
+            SixBtn.Click += SixBtn_Click;
+            // 
+            // ThreeBtn
+            // 
+            ThreeBtn.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            ThreeBtn.ForeColor = Color.Black;
+            ThreeBtn.Location = new Point(170, 93);
+            ThreeBtn.Name = "ThreeBtn";
+            ThreeBtn.Size = new Size(69, 64);
+            ThreeBtn.TabIndex = 9;
+            ThreeBtn.Text = "3";
+            ThreeBtn.UseVisualStyleBackColor = true;
+            ThreeBtn.Click += ThreeBtn_Click;
+            // 
+            // MinusPlusBtn
+            // 
+            MinusPlusBtn.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            MinusPlusBtn.ForeColor = Color.Black;
+            MinusPlusBtn.Location = new Point(317, 303);
+            MinusPlusBtn.Name = "MinusPlusBtn";
+            MinusPlusBtn.Size = new Size(69, 64);
+            MinusPlusBtn.TabIndex = 16;
+            MinusPlusBtn.Text = "-/+";
+            MinusPlusBtn.UseVisualStyleBackColor = true;
+            MinusPlusBtn.Click += MinusPlusBtn_Click;
+            // 
+            // DivideBtn
+            // 
+            DivideBtn.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            DivideBtn.ForeColor = Color.Black;
+            DivideBtn.Location = new Point(317, 233);
+            DivideBtn.Name = "DivideBtn";
+            DivideBtn.Size = new Size(69, 64);
+            DivideBtn.TabIndex = 15;
+            DivideBtn.Text = "/";
+            DivideBtn.UseVisualStyleBackColor = true;
+            DivideBtn.Click += DivideBtn_Click;
+            // 
+            // MinusBtn
+            // 
+            MinusBtn.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            MinusBtn.ForeColor = Color.Black;
+            MinusBtn.Location = new Point(317, 163);
+            MinusBtn.Name = "MinusBtn";
+            MinusBtn.Size = new Size(69, 64);
+            MinusBtn.TabIndex = 14;
+            MinusBtn.Text = "-";
+            MinusBtn.UseVisualStyleBackColor = true;
+            MinusBtn.Click += MinusBtn_Click;
+            // 
+            // ClearBtn
+            // 
+            ClearBtn.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            ClearBtn.ForeColor = Color.Black;
+            ClearBtn.Location = new Point(317, 93);
+            ClearBtn.Name = "ClearBtn";
+            ClearBtn.Size = new Size(144, 64);
+            ClearBtn.TabIndex = 13;
+            ClearBtn.Text = "Clear";
+            ClearBtn.UseVisualStyleBackColor = true;
+            ClearBtn.Click += ClearBtn_Click;
+            // 
+            // ModuleBtn
+            // 
+            ModuleBtn.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            ModuleBtn.ForeColor = Color.Black;
+            ModuleBtn.Location = new Point(392, 303);
+            ModuleBtn.Name = "ModuleBtn";
+            ModuleBtn.Size = new Size(69, 64);
+            ModuleBtn.TabIndex = 20;
+            ModuleBtn.Text = "%";
+            ModuleBtn.UseVisualStyleBackColor = true;
+            ModuleBtn.Click += ModuleBtn_Click;
+            // 
+            // MultiplyBtn
+            // 
+            MultiplyBtn.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            MultiplyBtn.ForeColor = Color.Black;
+            MultiplyBtn.Location = new Point(392, 233);
+            MultiplyBtn.Name = "MultiplyBtn";
+            MultiplyBtn.Size = new Size(69, 64);
+            MultiplyBtn.TabIndex = 19;
+            MultiplyBtn.Text = "X";
+            MultiplyBtn.UseVisualStyleBackColor = true;
+            MultiplyBtn.Click += MultiplyBtn_Click;
+            // 
+            // PlusBtn
+            // 
+            PlusBtn.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            PlusBtn.ForeColor = Color.Black;
+            PlusBtn.Location = new Point(392, 163);
+            PlusBtn.Name = "PlusBtn";
+            PlusBtn.Size = new Size(69, 64);
+            PlusBtn.TabIndex = 18;
+            PlusBtn.Text = "+";
+            PlusBtn.UseVisualStyleBackColor = true;
+            PlusBtn.Click += PlusBtn_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(518, 73);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(223, 294);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 21;
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(64, 0, 64);
+            ClientSize = new Size(773, 450);
+            Controls.Add(pictureBox1);
+            Controls.Add(ModuleBtn);
+            Controls.Add(MultiplyBtn);
+            Controls.Add(PlusBtn);
+            Controls.Add(MinusPlusBtn);
+            Controls.Add(DivideBtn);
+            Controls.Add(MinusBtn);
+            Controls.Add(ClearBtn);
+            Controls.Add(EqualsBtn);
+            Controls.Add(NineBtn);
+            Controls.Add(SixBtn);
+            Controls.Add(ThreeBtn);
+            Controls.Add(CommaBtn);
+            Controls.Add(EightBtn);
+            Controls.Add(FiveBtn);
+            Controls.Add(TwoBtn);
+            Controls.Add(ZeroBtn);
+            Controls.Add(SevenBtn);
+            Controls.Add(FourBtn);
+            Controls.Add(OneBtn);
+            Controls.Add(TxtBox);
+            ForeColor = Color.White;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Calculator";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox TxtBox;
+        private Button OneBtn;
+        private Button FourBtn;
+        private Button SevenBtn;
+        private Button ZeroBtn;
+        private Button CommaBtn;
+        private Button EightBtn;
+        private Button FiveBtn;
+        private Button TwoBtn;
+        private Button EqualsBtn;
+        private Button NineBtn;
+        private Button SixBtn;
+        private Button ThreeBtn;
+        private Button MinusPlusBtn;
+        private Button DivideBtn;
+        private Button MinusBtn;
+        private Button ClearBtn;
+        private Button ModuleBtn;
+        private Button MultiplyBtn;
+        private Button PlusBtn;
+        private PictureBox pictureBox1;
+    }
+}
